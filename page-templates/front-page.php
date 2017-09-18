fafb085e737695422801b2b9b526a99f40266608
Maju Comendador <maju.comendador@webee.com.mt>
2017-09-19 05:17:56 +0800

    First Commit

---
+++ page-templates/front-page.php
@@ -0,0 +1,19 @@
+<?php
+
+//Template Name: Front Page
+
+get_header();
+
+?>
+
+<section class="homepage-wrapper">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Front page</h1>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
