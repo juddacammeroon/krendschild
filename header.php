de311e6d383f1c8197a44f7ef5a5744e1a665f5a
Maju Comendador <maju.comendador@gmail.com>
2017-10-26 23:40:46 +0800

    header.php updated

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
     <?php if(!is_page_template( 'blank-page.php' ) && !is_page_template( 'blank-page-with-container.php' )): ?>
 	<header id="masthead" class="site-header navbar-static-top" role="banner">
         <div class="container">
-            <nav class="navbar navbar-toggleable-md navbar-light">
+            <nav class="navbar navbar-expand-lg navbar-light">
 
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
